f6309dc9c30e220ae4f901e1d9907d62e891e548
EC2 Default User <ec2-user@ip-172-31-66-103.ec2.internal>
2021-07-04 19:15:22 +0000

    rubyの勉強

---
+++ lib/text2.rb
@@ -0,0 +1,28 @@
+# upcaseとはローマ字をすべて大文字にするメソッド
+# 長い文章をしまえる(<<任意の文字)
+b = <<TEXT.upcase
+hello,
+good-bye.
+TEXT
+puts b
+# 最期の識別子をインデントさせることができる(つけずにインデントすると使えなくなる)
+def a
+  <<-TE.upcase
+hello,
+good-bye
+  TE
+end
+puts a
+# 出力時内部文字列のインデントが出力時無効に
+c = <<~TXT.upcase
+ hello,
+ good-bye.
+TXT
+puts c
+# prependとは渡された文字列を先頭に配置するメソッド
+a = "Ruby"
+a.prepend(<<TETE)
+Java
+PHP
+TETE
+puts a
---
+++ sample.rb
@@ -0,0 +1,12 @@
+puts 1+2
+
+a = "わはは"
+puts a
+
+b = "がはは"
+puts b
+
+# ここはこめんと
+# ここもこめんと
+name = "AYAKA"
+puts "Hello,#{name}"
